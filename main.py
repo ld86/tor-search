d0cbe5558e30fba3a159e077e976ef61d4a8ba90
Bogdan Melnik <teh.ld86@gmail.com>
2015-01-03 12:24:53 +0000

    Basic crawling

--- main.py
+++ main.py
@@ -1,51 +1,56 @@
 import socks
+import socket
+
+
+def create_connection(address, timeout=None, source_address=None):
+    sock = socks.socksocket()
+    address = (address[0].encode('utf-8'), address[1])
+    sock.connect(address)
+    return sock
+
+socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 9050)
+socket.socket = socks.socksocket
+socket.create_connection = create_connection
+
 import Queue
-import select
 from BeautifulSoup import BeautifulSoup
 from urlparse import urlparse
 import re
-
-# s.connect(('zqktlwi4fecvo6ri.onion', 80))
-# s.sendall("GET /wiki/Main_Page/ HTTP/1.1\r\nHost: 3fyb44wdhnd2ghhl.onion\r\n\r\n")
+import urllib2
 
 
 class Crawler:
 
     def __init__(self):
-        socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 9050, True)
-        self.s = socks.socksocket()
         self.queue = Queue.Queue()
+        self.was = set()
 
     def serve(self):
         while not self.queue.empty():
             task = self.queue.get()
             self.crawl(task)
 
-    def readall(self, s):
-        c = []
-        while True:
-            r, _, _ = select.select([s], [], [], 10)
-            if not r:
-                return "".join(c)
-            c.append(s.recv(2 ** 16))
-
     def crawl(self, task):
-        s = self.s
-        s.connect((task.host, 80))
-        s.sendall("GET {0} HTTP/1.1\r\nHost: {1}\r\n\r\n".format(task.path, task.host))
-        task.content = self.readall(s)
-        s.close()
+        if task.url() in self.was or not task.scheme.startswith("http"):
+            return
+        self.was.add(task.url())
 
+        print("[>] {0}".format(task.url()))
+        try:
+
+            task.content = urllib2.urlopen(task.url())
+        except socks.GeneralProxyError as e:
+            print("[!] {0} : {1}".format(task.url(), e))
+            return
         self.parse(task)
 
     def parse(self, task):
         bs = BeautifulSoup(task.content)
         allA = bs.findAll('a', attrs={'href': re.compile(r'\.onion')})
-        print("[!] {0} : {1}".format(task.host + task.path, len(allA)))
+        print("[<] {0} : {1}".format(task.url(), len(allA)))
         for a in allA:
             href = a['href']
-            url = urlparse(href)
-            print(url)
+            self.queue.put(Task(href))
 
     def add_task(self, task):
         self.queue.put(task)
@@ -53,14 +58,20 @@ class Crawler:
 
 class Task:
 
-    def __init__(self, host, path):
-        self.host = host
-        self.path = path
+    def __init__(self, url):
+        url = urlparse(url)
+        self.scheme = url.scheme + "://"
+        self.host = url.netloc
+        self.path = '/' if url.path == '' else url.path
+
+    def url(self):
+        return self.scheme + self.host + self.path
 
 
 def main():
     crawler = Crawler()
-    crawler.add_task(Task("zqktlwi4fecvo6ri.onion", "/wiki/Main_Page"))
+    crawler.add_task(Task("http://zqktlwi4fecvo6ri.onion/wiki/Main_Page"))
+    # crawler.add_task(Task("http://cyjabr4pfzupo7pg.onion/"))
     crawler.serve()
 
 
